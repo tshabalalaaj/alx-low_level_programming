b3efdb5443883d40f4efc1fcb15d0a8ec47c7272
tshabalalaaj <tshabalalaaj@outlook.com>
2021-08-26 07:22:30 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-  * Prints a text
-  * Returns 0 when successfull
-  */
+ * main - Prints string
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
