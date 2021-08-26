623a07a9caf742173e5a65a4c5d4736e5e2ab6f9
tshabalalaaj <tshabalalaaj@outlook.com>
2021-08-26 07:04:41 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-/*
- * Print text
- *
- * return success
- */
+#include <stdio.h>
+/**
+  * Prints a text
+  * Returns 0 when successfull
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
+
 	return (0);
 }
