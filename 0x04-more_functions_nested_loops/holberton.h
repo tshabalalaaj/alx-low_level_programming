9ddfe08d13f09dc56a6d730425c31ae7d7a155c7
tshabalalaaj <tshabalalaaj@outlook.com>
2021-09-02 17:26:20 +0200

    All

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,4 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+
+#endif
