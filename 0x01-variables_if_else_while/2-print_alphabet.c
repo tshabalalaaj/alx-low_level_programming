55342de9d664d5a16e28c76d29af29719e34407e
tshabalalaaj <tshabalalaaj@outlook.com>
2021-09-04 04:20:54 -0700

    All

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,13 @@
 /* Lowercase*/
 int main(void)
 {
-	char c = 'H';
+	char c;
 	char s = '\n';
+	for (c = 65; c <= 90; c++)
+	{
 	char result = tolower(c);
 	putchar(result);
+	}
 	putchar(s);
 	return 0;
 }
