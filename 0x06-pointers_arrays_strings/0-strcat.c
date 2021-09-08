ed1d43ff567f2a7c2c7e405f838ed32dbcf31ebc
tshabalalaaj <tshabalalaaj@outlook.com>
2021-09-08 16:26:59 +0000

    All

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <string.h>
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,7 @@
+#include <string.h>
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return dest;
+}
